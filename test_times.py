f5c7a99eb6b1de80fa11ff87356cfed94f01fdc6
Anqi Chen <rmapac1@ucl.ac.uk>
2025-10-27 09:58:45 +0000

    debug-times.py

--- test_times.py
+++ test_times.py
@@ -1,8 +1,28 @@
+import datetime
+from times import time_range, compute_overlap_time
 
-from times import compute_overlap_time, time_range
 
-def test_generic_case():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
-    assert compute_overlap_time(large, short) == expected
+def test_non_overlapping_ranges():
+    """Two time ranges that do not overlap"""
+    range1 = time_range("2025-10-26 10:00:00", "2025-10-26 11:00:00")
+    range2 = time_range("2025-10-26 12:00:00", "2025-10-26 13:00:00")
+    overlap = compute_overlap_time(range1, range2)
+    assert overlap == []
+
+
+def test_multiple_intervals_with_overlap():
+    """Two time ranges with several intervals each, overlapping"""
+    range1 = time_range("2025-10-26 10:00:00", "2025-10-26 12:00:00", number_of_intervals=2)
+    range2 = time_range("2025-10-26 11:00:00", "2025-10-26 13:00:00", number_of_intervals=2)
+    overlap = compute_overlap_time(range1, range2)
+    # At least one overlap should exist
+    assert any(start < end for start, end in overlap)
+
+
+def test_end_starts_exactly_same_time():
+    """Two ranges where one ends exactly when the other starts"""
+    range1 = time_range("2025-10-26 10:00:00", "2025-10-26 11:00:00")
+    range2 = time_range("2025-10-26 11:00:00", "2025-10-26 12:00:00")
+    overlap = compute_overlap_time(range1, range2)
+    assert overlap == []
+
